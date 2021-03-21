52cdc537cfb8d1cb99208f4cbe5e1a582b44561e
むずでょ <muzudho1@gmail.com>
2021-03-21 19:26:27 +0900

    base_theta

--- c_step1/make_gif.py
+++ c_step1/make_gif.py
@@ -4,107 +4,114 @@ from PIL import Image
 from datetime import datetime
 import math
 
-# 色 BGR
-white = (255, 255, 255)
-black = (16, 16, 16)
-red = (250, 100, 100)
-green = (100, 250, 100)
-blue = (100, 100, 250)
 
-images = []
+def make_gif(base_theta):
+    # 色 BGR
+    # white = (255, 255, 255)
+    black = (16, 16, 16)
+    red = (250, 100, 100)
+    green = (100, 250, 100)
+    blue = (100, 100, 250)
 
-# 描画する画像を作る,128を変えると色を変えれます 0黒→255白
-canvas_width = 800
-canvas_height = 400
-channels = 3
-background = 255
+    images = []
 
-# キャンバス
-canvas = np.full((canvas_height, canvas_width, channels),
-                 background, dtype=np.uint8)
+    # 描画する画像を作る,128を変えると色を変えれます 0黒→255白
+    canvas_width = 800
+    canvas_height = 400
+    channels = 3
+    background = 255
 
-bar_top = 40
-bar_max_height = 340
-bar_bottom = bar_top + bar_max_height
+    # キャンバス
+    canvas = np.full((canvas_height, canvas_width, channels),
+                     background, dtype=np.uint8)
 
-circle_center = (200, 200)  # x, y
-circle_range = 140
-color_pallete_range = 175
+    bar_top = 40
+    bar_max_height = 340
+    bar_bottom = bar_top + bar_max_height
 
-# 円、描画する画像を指定、座標（x,y),半径、色、線の太さ（-1は塗りつぶし）
-cv2.circle(canvas, circle_center, circle_range, black, thickness=2)
+    circle_center = (200, 200)  # x, y
+    circle_range = 140
+    color_pallete_range = 175
 
-# 点R
-theta = 0
-pr = (int(circle_range * math.sin(math.radians(theta)) + circle_center[0]),
-      int(-circle_range * math.cos(math.radians(theta)) + circle_center[1]))  # yは上下反転
-cv2.circle(canvas, pr, 10, red, thickness=-1)
+    # 円、描画する画像を指定、座標（x,y),半径、色、線の太さ（-1は塗りつぶし）
+    cv2.circle(canvas, circle_center, circle_range, black, thickness=2)
 
-# 点G
-theta = 240  # 時計回り
-pg = (int(circle_range * math.sin(math.radians(theta)) + circle_center[0]),
-      int(-circle_range * math.cos(math.radians(theta)) + circle_center[1]))  # yは上下反転
-cv2.circle(canvas, pg, 10, green, thickness=-1)
+    # 点R
+    theta = (0+base_theta) % 360
+    pr = (int(circle_range * math.sin(math.radians(theta)) + circle_center[0]),
+          int(-circle_range * math.cos(math.radians(theta)) + circle_center[1]))  # yは上下反転
+    cv2.circle(canvas, pr, 10, red, thickness=-1)
 
-# 点B
-theta = 120
-pb = (int(circle_range * math.sin(math.radians(theta)) + circle_center[0]),
-      int(-circle_range * math.cos(math.radians(theta)) + circle_center[1]))  # yは上下反転
-cv2.circle(canvas, pb, 10, blue, thickness=-1)
+    # 点G
+    theta = (240+base_theta) % 360  # 時計回り
+    pg = (int(circle_range * math.sin(math.radians(theta)) + circle_center[0]),
+          int(-circle_range * math.cos(math.radians(theta)) + circle_center[1]))  # yは上下反転
+    cv2.circle(canvas, pg, 10, green, thickness=-1)
 
-# バーの筋
-barr_x = 450
-barg_x = 550
-barb_x = 650
+    # 点B
+    theta = (120+base_theta) % 360
+    pb = (int(circle_range * math.sin(math.radians(theta)) + circle_center[0]),
+          int(-circle_range * math.cos(math.radians(theta)) + circle_center[1]))  # yは上下反転
+    cv2.circle(canvas, pb, 10, blue, thickness=-1)
 
-# 水平線R
-# 線、描画する画像を指定、座標1点目、2点目、色、線の太さ
-cv2.line(canvas, (pr[0], pr[1]), (barr_x, pr[1]), red, thickness=2)
+    # バーの筋
+    barr_x = 450
+    barg_x = 550
+    barb_x = 650
 
-# 水平線G
-cv2.line(canvas, (pg[0], pg[1]), (barg_x, pg[1]), green, thickness=2)
+    # 水平線R
+    # 線、描画する画像を指定、座標1点目、2点目、色、線の太さ
+    cv2.line(canvas, (pr[0], pr[1]), (barr_x, pr[1]), red, thickness=2)
 
-# 水平線B
-cv2.line(canvas, (pb[0], pb[1]), (barb_x, pb[1]), blue, thickness=2)
+    # 水平線G
+    cv2.line(canvas, (pg[0], pg[1]), (barg_x, pg[1]), green, thickness=2)
 
-# バーR
-bar_width = 98
-barr_p1 = (barr_x, pr[1])
-barr_p2 = (barr_x+bar_width, bar_bottom)
-cv2.rectangle(canvas, barr_p1, barr_p2, red, thickness=-1)
+    # 水平線B
+    cv2.line(canvas, (pb[0], pb[1]), (barb_x, pb[1]), blue, thickness=2)
 
-# バーG
-barg_p1 = (barg_x, pg[1])
-barg_p2 = (barg_x+bar_width, bar_bottom)
-cv2.rectangle(canvas, barg_p1, barg_p2, green, thickness=-1)
+    # バーR
+    bar_width = 98
+    barr_p1 = (barr_x, pr[1])
+    barr_p2 = (barr_x+bar_width, bar_bottom)
+    cv2.rectangle(canvas, barr_p1, barr_p2, red, thickness=-1)
 
-# バーB
-barb_p1 = (barb_x, pb[1])
-barb_p2 = (barb_x+bar_width, bar_bottom)
-cv2.rectangle(canvas, barb_p1, barb_p2, blue, thickness=-1)
+    # バーG
+    barg_p1 = (barg_x, pg[1])
+    barg_p2 = (barg_x+bar_width, bar_bottom)
+    cv2.rectangle(canvas, barg_p1, barg_p2, green, thickness=-1)
 
-# 色円
-valurr = 255-int(pr[1]/bar_max_height*255)
-valurg = 255-int(pg[1]/bar_max_height*255)
-valurb = 255-int(pb[1]/bar_max_height*255)
-color = (valurr, valurg, valurb)
-print(f"color={color}")
-theta = 0
-pr = (int(color_pallete_range * math.sin(math.radians(theta)) + circle_center[0]),
-      int(-color_pallete_range * math.cos(math.radians(theta)) + circle_center[1]))  # yは上下反転
-cv2.circle(canvas, pr, 20, color, thickness=-1)
+    # バーB
+    barb_p1 = (barb_x, pb[1])
+    barb_p2 = (barb_x+bar_width, bar_bottom)
+    cv2.rectangle(canvas, barb_p1, barb_p2, blue, thickness=-1)
 
-images.append(Image.fromarray(canvas))
+    # 色円
+    valurr = 255-int(pr[1]/bar_max_height*255)
+    valurg = 255-int(pg[1]/bar_max_height*255)
+    valurb = 255-int(pb[1]/bar_max_height*255)
+    color = (valurr, valurg, valurb)
+    print(f"color={color}")
+    theta = base_theta
+    pr = (int(color_pallete_range * math.sin(math.radians(theta)) + circle_center[0]),
+          int(-color_pallete_range * math.cos(math.radians(theta)) + circle_center[1]))  # yは上下反転
+    cv2.circle(canvas, pr, 20, color, thickness=-1)
 
-# cv2.imshow('canvas',canvas)
-# cv2.imwrite('form.jpg',canvas)
-date = datetime.now().strftime("%Y%m%d-%H%M%S")
-path = f"out-{date}.gif"
-fps = 1
-duration_time = int(1000.0 / fps)
-images[0].save(path,
-               save_all=True,
-               append_images=images[1:],
-               optimize=False,
-               duration=duration_time,
-               loop=0)
+    images.append(Image.fromarray(canvas))
+
+    # cv2.imshow('canvas',canvas)
+    # cv2.imwrite('form.jpg',canvas)
+    date = datetime.now().strftime("%Y%m%d-%H%M%S")
+    path = f"out-{date}-{base_theta}.gif"
+    fps = 1
+    duration_time = int(1000.0 / fps)
+    images[0].save(path,
+                   save_all=True,
+                   append_images=images[1:],
+                   optimize=False,
+                   duration=duration_time,
+                   loop=0)
+
+
+for i in range(0, 36):
+    theta = 10*i
+    make_gif(theta)

a7beb7ee05db704a8fbbfd3afefa59248972aa90
むずでょ <muzudho1@gmail.com>
2021-03-21 15:59:00 +0900

    point

--- c_step1/make_gif.py
+++ c_step1/make_gif.py
@@ -2,21 +2,48 @@ import cv2
 import numpy as np
 from PIL import Image
 from datetime import datetime
+import math
 
 # 色 BGR
 black = (16, 16, 16)
+red = (250, 100, 100)
+green = (100, 250, 100)
+blue = (100, 100, 250)
 
 images = []
 
 # 描画する画像を作る,128を変えると色を変えれます 0黒→255白
 width = 800
 height = 400
-canvas = np.full((height, width, 3), 255, dtype=np.uint8)
+channels = 3
+background = 255
 
 # 円、描画する画像を指定、座標（x,y),半径、色、線の太さ（-1は塗りつぶし）
+canvas = np.full((height, width, channels), background, dtype=np.uint8)
 cv2.circle(canvas, (200, 200), 180, black, thickness=2)
-images.append(Image.fromarray(canvas))
 
+# 点R
+cx = 200
+cy = 200
+range = 180
+theta = 0
+x = int(range * math.sin(math.radians(theta)) + cx)
+y = int(-range * math.cos(math.radians(theta)) + cy)  # yは上下反転
+cv2.circle(canvas, (x, y), 10, red, thickness=-1)
+
+# 点G
+theta = 240  # 時計回り
+x = int(range * math.sin(math.radians(theta)) + cx)
+y = int(-range * math.cos(math.radians(theta)) + cy)  # yは上下反転
+cv2.circle(canvas, (x, y), 10, green, thickness=-1)
+
+# 点B
+theta = 120
+x = int(range * math.sin(math.radians(theta)) + cx)
+y = int(-range * math.cos(math.radians(theta)) + cy)  # yは上下反転
+cv2.circle(canvas, (x, y), 10, blue, thickness=-1)
+
+images.append(Image.fromarray(canvas))
 
 # cv2.imshow('canvas',canvas)
 # cv2.imwrite('form.jpg',canvas)

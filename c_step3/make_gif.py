0f962e81798647509893ade1317a99c04102471a
むずでょ <muzudho1@gmail.com>
2021-03-23 07:31:17 +0900

    箱を３段にしたいぜ（＾～＾）

--- c_step3/make_gif.py
+++ c_step3/make_gif.py
@@ -17,8 +17,8 @@ def make_gif(base_theta):
 
     # 描画する画像を作る
     # 横幅 約500 以上にすると ブログで縮小されて .gif ではなくなるので、横幅を 約500未満にすること（＾～＾）
-    canvas_width = 400
-    canvas_height = 280
+    canvas_width = 512
+    canvas_height = 384
     channels = 3
     # 0黒→255白
     background = 255
@@ -32,9 +32,14 @@ def make_gif(base_theta):
     color_pallete_range = 120
     color_pallete_circle_range = 20
 
-    bar_top = circle_center[1] - circle_range
-    bar_max_height = 2*circle_range
-    bar_bottom = bar_top + bar_max_height
+    bar_top1 = circle_center[1] - circle_range
+    bar_box_height1 = 0
+    bar_top2 = bar_top1 + bar_box_height1
+    bar_box_height2 = 2*circle_range
+    bar_top3 = bar_top2 + bar_box_height2
+    bar_box_height3 = 0
+    bar_bottom = bar_top3 + bar_box_height3
+    bar_box_height = bar_box_height1 + bar_box_height2 + bar_box_height3
 
     # 水平線グリッド
     grid_interval = 16
@@ -80,8 +85,8 @@ def make_gif(base_theta):
     # 水平線B
     cv2.line(canvas, (pb[0], pb[1]), (barb_x, pb[1]), blue, thickness=2)
 
-    # RGBバー領域
-    bar_area_p1 = (barr_x, bar_top)
+    # RGBバー(中部)領域
+    bar_area_p1 = (barr_x, bar_top2)
     bar_area_p2 = (barb_x+bar_width, bar_bottom)
     cv2.rectangle(canvas, bar_area_p1, bar_area_p2, light_gray, thickness=4)
 
@@ -101,9 +106,9 @@ def make_gif(base_theta):
     cv2.rectangle(canvas, barb_p1, barb_p2, blue, thickness=-1)
 
     # 色値
-    valurr = 255-int((pr[1]-bar_top)/bar_max_height*255)
-    valurg = 255-int((pg[1]-bar_top)/bar_max_height*255)
-    valurb = 255-int((pb[1]-bar_top)/bar_max_height*255)
+    valurr = 255-int((pr[1]-bar_top1)/bar_box_height*255)
+    valurg = 255-int((pg[1]-bar_top1)/bar_box_height*255)
+    valurb = 255-int((pb[1]-bar_top1)/bar_box_height*255)
 
     # R値テキスト
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -139,7 +144,7 @@ def make_gif(base_theta):
     # 色円
     color = (valurr, valurg, valurb)
     # print(f"({pr[1]},{pg[1]},{pb[1]})")
-    print(f"({pr[1]-bar_top},{pg[1]-bar_top},{pb[1]-bar_top})")
+    # print(f"({pr[1]-bar_top},{pg[1]-bar_top},{pb[1]-bar_top})")
     # print(
     #    f"color={color} ({int(pr[1]/bar_max_height*255)},{int(pg[1]/bar_max_height*255)},{int(pb[1]/bar_max_height*255)})")
     # print(
@@ -154,8 +159,8 @@ def make_gif(base_theta):
 
 images = []
 
-for i in range(0, 36):
-    theta = 10*i
+for i_frame in range(0, 36):
+    theta = 10*i_frame
     image = make_gif(theta)
     images.append(image)
 

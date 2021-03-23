347249b7a2836726b1242d6a2125cae96f37479a
むずでょ <muzudho1@gmail.com>
2021-03-23 19:36:00 +0900

    coding

--- e_step2/pygame00.py
+++ e_step2/pygame00.py
@@ -8,25 +8,25 @@ from pygame.locals import QUIT
 # pygame の初期化
 pygame.init()
 
-display_size = (400, 300)  # width, height
-sf = pygame.display.set_mode(display_size)  # アプリケーションウィンドウ
+DISPLAY_SIZE = (400, 300)  # width, height
+SURFACE = pygame.display.set_mode(DISPLAY_SIZE)  # アプリケーションウィンドウ
 pygame.display.set_caption('Application: pygame00.py')
 
-fps = pygame.time.Clock()  # フレームレート制御のための Clock オブジェクト
+FPS = pygame.time.Clock()  # フレームレート制御のための Clock オブジェクト
 
 # 画像の読み込み
-im1 = pygame.image.load('./e_step1/img/ball01.png')
-im1_w = im1.get_width()  # 画像の横幅の取得
-im1_h = im1.get_height()  # 画像の高さの取得
+IMAGE1 = pygame.image.load('./e_step1/img/ball01.png')
+IMAGE1_W = IMAGE1.get_width()  # 画像の横幅の取得
+IMAGE1_H = IMAGE1.get_height()  # 画像の高さの取得
 
 # メインループ
-white = (255, 255, 255)
-ballp = [0, 0]  # x, y
-dx = 3
-dy = 2  # 移動量
+WHITE = (255, 255, 255)
+BALLP = [0, 0]  # x, y
+DX = 3
+DY = 2  # 移動量
 while True:
-    sf.fill(white)  # 背景の色
-    sf.blit(im1, (ballp[0], ballp[1]))  # ボールの描画
+    SURFACE.fill(WHITE)  # 背景の色
+    SURFACE.blit(IMAGE1, (BALLP[0], BALLP[1]))  # ボールの描画
     # イベントキューを処理するループ
     for ev in pygame.event.get():
 
@@ -37,19 +37,19 @@ while True:
     # ディスプレイの更新
     pygame.display.update()
     # フレームレートの設定
-    fps.tick(30)  # 30 FPS に設定
+    FPS.tick(30)  # 30 FPS に設定
     # ボール移動（位置変更）の処理
-    ballp[0] += dx
-    ballp[1] += dy  # 移 動
-    if ballp[0] + im1_w > display_size[0]:  # ボールが右端に衝突した場合の処理
-        ballp[0] = display_size[0] - im1_w - 1
-        dx *= -1
-    elif ballp[0] < 0:  # ボールが左端に衝突した場合の処理
-        ballp[0] = 0
-        dx *= -1
-    if ballp[1] + im1_h > display_size[1]:  # ボールが床に衝突した場合の処理
-        ballp[1] = display_size[1] - im1_h - 1
-        dy *= -1
-    elif ballp[1] < 0:  # ボールが天井に衝突した場合の処理
-        ballp[1] = 0
-        dy *= -1
+    BALLP[0] += DX
+    BALLP[1] += DY  # 移 動
+    if BALLP[0] + IMAGE1_W > DISPLAY_SIZE[0]:  # ボールが右端に衝突した場合の処理
+        BALLP[0] = DISPLAY_SIZE[0] - IMAGE1_W - 1
+        DX *= -1
+    elif BALLP[0] < 0:  # ボールが左端に衝突した場合の処理
+        BALLP[0] = 0
+        DX *= -1
+    if BALLP[1] + IMAGE1_H > DISPLAY_SIZE[1]:  # ボールが床に衝突した場合の処理
+        BALLP[1] = DISPLAY_SIZE[1] - IMAGE1_H - 1
+        DY *= -1
+    elif BALLP[1] < 0:  # ボールが天井に衝突した場合の処理
+        BALLP[1] = 0
+        DY *= -1

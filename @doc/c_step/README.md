4272c449e81f7cef587d4adc9cfffa613af580c3
むずでょ <muzudho1@gmail.com>
2021-03-24 20:04:01 +0900

    calc_step1

--- @doc/c_step/README.md
+++ @doc/c_step/README.md
@@ -5,3 +5,5 @@
 ![20210324color14.png](./img/20210324color14.png)  
 
 ![20210324color15.png](./img/20210324color15.png)  
+
+![20210324color16.png](./img/20210324color16.png)  
--- c_step7/color_calc.py
+++ c_step7/color_calc.py
@@ -4,6 +4,13 @@
 import math
 
 
+def calc_step1(theta):
+    red_y = (math.cos(math.radians(theta))+1)/2
+    green_y = (math.cos(math.radians(theta-120))+1)/2
+    blue_y = (math.cos(math.radians(theta+120))+1)/2
+    return (red_y, green_y, blue_y)
+
+
 def calc_color(theta, bar_rate):
     """(r,g,b)を返します
     """
@@ -11,9 +18,10 @@ def calc_color(theta, bar_rate):
     # 下駄
     offset = 255*bar_rate[2]
 
-    red_y = 255*bar_rate[1]*math.cos(math.radians(theta))
-    green_y = 255*bar_rate[1]*math.cos(math.radians(theta-120))
-    blue_y = 255*bar_rate[1]*math.cos(math.radians(theta+120))
+    color = calc_step1(theta)
+    red_y = 255*bar_rate[1]*color[0]
+    green_y = 255*bar_rate[1]*color[1]
+    blue_y = 255*bar_rate[1]*color[2]
 
     red_y = int(red_y+offset)
     green_y = int(green_y+offset)

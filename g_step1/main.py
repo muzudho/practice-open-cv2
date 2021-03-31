1639368274b6a9cffceea8f1576a7c4c23635f46
むずでょ <muzudho1@gmail.com>
2021-03-31 20:30:18 +0900

    正三角形を描く練習

--- g_step1/main.py
+++ g_step1/main.py
@@ -15,44 +15,43 @@ BLUE = (100, 100, 250)
 
 # 描画する画像を作る
 # 横幅 約500 以上にすると ブログで縮小されて .gif ではなくなるので、横幅を 約500未満にすること（＾～＾）
-CANVAS_WIDTH = 400
-CANVAS_HEIGHT = 400
+CANVAS_WIDTH = 500
+CANVAS_HEIGHT = 500
 CHANNELS = 3
 # モノクロ背景 0黒→255白
 MONO_BACKGROUND = 255
 
 
 def main():
-    """一周分の画像を出力
+    """画像を出力
     """
-
-    arc_count = 8
-    circumference = 360  # 半径１の円の一周の長さ
-    unit_arc = circumference/arc_count  # 等分割した１つの弧
-
-    theta = 0
 
     # キャンバス
     canvas = np.full((CANVAS_HEIGHT, CANVAS_WIDTH, CHANNELS),
                      MONO_BACKGROUND, dtype=np.uint8)
 
-    center = (200, 200)  # x, y
-    box_size = (100, 100)
-    # color = RED
-    # color = (-255, -255, -255) # 黒
-    # color = (-255, 0, 0) # 黒
-    color = (255, -255, 0)  # 赤 負値は切り捨て？
+    thichness = 2
 
-    # 円弧
-    # 楕円、描画する画像を指定、座標(x,y),xyの半径、角度,色、線の太さ(-1は塗りつぶし)
-    cv2.ellipse(canvas,
-                center,
-                box_size,
-                0,
-                theta,
-                theta+unit_arc,
-                color,
-                thickness=4)
+    # 平行する２本の直線a, b
+    # a
+    cv2.line(canvas,
+             (10, 100),
+             (400, 100),
+             PALE_GRAY,
+             thickness=thichness)
+    # b
+    cv2.line(canvas,
+             (10, 300),
+             (400, 300),
+             PALE_GRAY,
+             thickness=thichness)
+
+    # ある点c
+    cv2.circle(canvas,
+               (200, 200),
+               5,
+               PALE_GRAY,
+               thickness=-1)  # thichness=-1 は塗りつぶし
 
     # cv2.imshow('Title', canvas)
     # cv2.imwrite('form.jpg',canvas)
@@ -62,7 +61,7 @@ def main():
     # BGRをRGBにする
     canvas = cv2.cvtColor(canvas, cv2.COLOR_BGR2RGB)
 
-    cv2.imwrite(f"./shared/out-f_step1.png", canvas)
+    cv2.imwrite(f"./shared/out-g_step1.png", canvas)
 
 
 main()

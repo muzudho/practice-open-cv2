b0a125f47f9c90fec9dc253efba7d669c7af90d8
むずでょ <muzudho1@gmail.com>
2021-04-03 18:36:51 +0900

    WIP 式を簡単にしている途中

--- c_step19/color_model.py
+++ c_step19/color_model.py
@@ -27,39 +27,51 @@ def to_color_rate(vertical_parcent, theta):
         f"n3y_on_circumference=({n3y_on_circumference[0]}, {n3y_on_circumference[1]}, \
 {n3y_on_circumference[2]})")
 
-    # -1.0 ～ 1.0 を 0 ～ 2.0 に変換してから長短を調べます
+    # -1.0 ～ 1.0 に +1.0 を足して 0.0 ～ 2.0 に変形し、
+    # 扱いやすいように 2 で割って 0.0 ～ 1.1 にします
     n3y_in_diameter = (
-        n3y_on_circumference[0] + 1.0,
-        n3y_on_circumference[1] + 1.0,
-        n3y_on_circumference[2] + 1.0)
-    if not 0.0 <= n3y_in_diameter[0] <= 2.0:
+        (n3y_on_circumference[0] + 1.0) / 2,
+        (n3y_on_circumference[1] + 1.0) / 2,
+        (n3y_on_circumference[2] + 1.0) / 2)
+    print(
+        f"n3y_in_diameter=({n3y_in_diameter[0]:10.7f}, {n3y_in_diameter[1]:10.7f}, \
+{n3y_in_diameter[2]:10.7f})")
+    if not 0.0 <= n3y_in_diameter[0] <= 1.0:
         raise Exception(f"red={n3y_in_diameter[0]}")
-    if not 0.0 <= n3y_in_diameter[1] <= 2.0:
-        raise Exception(f"green={n3y_in_diameter[0]}")
-    if not 0.0 <= n3y_in_diameter[2] <= 2.0:
-        raise Exception(f"blue={n3y_in_diameter[0]}")
+    if not 0.0 <= n3y_in_diameter[1] <= 1.0:
+        raise Exception(f"green={n3y_in_diameter[1]}")
+    if not 0.0 <= n3y_in_diameter[2] <= 1.0:
+        raise Exception(f"blue={n3y_in_diameter[2]}")
+
     longest = max(n3y_in_diameter[0], n3y_in_diameter[1], n3y_in_diameter[2])
-    if not 0.0 <= longest <= 2.0:
-        raise Exception(f"longest={longest}")
+    if not 0.0 <= longest <= 1.0:
+        raise Exception(f"longest={longest:10.7f}")
     shortest = min(n3y_in_diameter[0], n3y_in_diameter[1], n3y_in_diameter[2])
-    if not 0.0 <= shortest <= 2.0:
-        raise Exception(f"shortest={shortest}")
+    if not 0.0 <= shortest <= 1.0:
+        raise Exception(f"shortest={shortest:10.7f}")
+
+    # shortest ～ x ～ longest という数は使いにくいので、 0.0 ～ 1.0 にフィッティングさせます。
+    # これは結局、 x を微調整するための働きです
+
     length = longest - shortest
-    if not 0.0 <= length <= 2.0:
+    if not 0.0 <= length <= 1.0:
         raise Exception(f"length={length}")
-    zoom = length / 2.0
-    if not 0.0 <= zoom <= 1.0:
-        raise Exception(f"zoom={zoom}")
-    print(f"longest={longest} shortest={shortest} length={length} zoom={zoom}")
+    print(f"longest={longest} shortest={shortest} length={length}")
 
     fit_to_diameter = (
-        __one_fit(n3y_in_diameter[0], zoom),
-        __one_fit(n3y_in_diameter[1], zoom),
-        __one_fit(n3y_in_diameter[2], zoom))
-
+        __one_fit(n3y_in_diameter[0], shortest, length),
+        __one_fit(n3y_in_diameter[1], shortest, length),
+        __one_fit(n3y_in_diameter[2], shortest, length))
     print(
-        f"fit_to_diameter=({fit_to_diameter[0]}, {fit_to_diameter[1]}, {fit_to_diameter[2]})")
-    print(f"vertical_parcent[2]={vertical_parcent[2]}")
+        f"fit_to_diameter=({fit_to_diameter[0]:10.7f}, {fit_to_diameter[1]:10.7f}, {fit_to_diameter[2]:10.7f})")
+    if not 0.0 <= fit_to_diameter[0] <= 1.0:
+        raise Exception(f"red={fit_to_diameter[0]}")
+    if not 0.0 <= fit_to_diameter[1] <= 1.0:
+        raise Exception(f"green={fit_to_diameter[1]}")
+    if not 0.0 <= fit_to_diameter[2] <= 1.0:
+        raise Exception(f"blue={fit_to_diameter[2]}")
+
+    # print(f"vertical_parcent[2]={vertical_parcent[2]}")
 
     # 'vertical_parcent[1]' - 箱全体に占める２段目の箱の縦幅の割合 0.0～1.0
     # 'vertical_parcent[2]' - 箱全体に占める３段目の箱の縦幅の割合 0.0～1.0
@@ -70,8 +82,8 @@ def to_color_rate(vertical_parcent, theta):
         fit_to_diameter[2] * vertical_parcent[1] + vertical_parcent[2])
 
 
-def __one_fit(rate, zoom):
+def __one_fit(rate, shortest, length):
     """フィットさせます"""
-    if zoom == 0:
+    if length == 0:
         return 0  # 0除算が起こるなら（仕方が無いので）差分は 0 にします
-    return rate / zoom
+    return (rate-shortest) / length

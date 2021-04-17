d14a0ac661020e99eddaaa23f9a7b616a3cffcbe
むずでょ <muzudho1@gmail.com>
2021-04-17 17:32:38 +0900

    search

--- i_step3/agent_move.py
+++ i_step3/agent_move.py
@@ -7,13 +7,13 @@ def move_up(board, agent):
     column = agent.location[0]
     row = agent.location[1] - 1
     if row >= 0 and not board.checked_rows[row][column]:
-        if board.rows[row][column] in ('┌', '┐', '│', '│┌', '┐│', '┘│', '│└', '┐┌'):
+        if board.rows[row][column] in ('┌', '┐', '┌r', '┐r', '│', '│┌', '┐│', '┘│', '│└', '┐┌'):
             # 道
             board.checked_rows[row][column] = True
             agent.location[1] -= 1
             return True
 
-        if len(board.rows[row][column]) == 1:
+        if len(board.rows[row][column]) == 1 or board.rows[row][column] == '..':
             # 文字
             board.checked_rows[row][column] = True
             agent.location[1] -= 1
@@ -33,12 +33,12 @@ def move_to_right(board, agent):
     row = agent.location[1]
     if column < board.width and not board.checked_rows[row][column]:
         # 道
-        if board.rows[row][column] in ('┐', '┘', '─', '─┌', '┐─', '┘─', '─└', '┘┐', '┐┘'):
+        if board.rows[row][column] in ('┐', '┘', '┐r', '┘r', '─', '─┌', '┐─', '┘─', '─└', '┘┐', '┐┘'):
             board.checked_rows[row][column] = True
             agent.location[0] += 1
             return True
 
-        if len(board.rows[row][column]) == 1:
+        if len(board.rows[row][column]) == 1 or board.rows[row][column] == '..':
             # 文字
             board.checked_rows[row][column] = True
             agent.location[0] += 1
@@ -58,12 +58,12 @@ def move_down(board, agent):
     row = agent.location[1] + 1
     if row < board.height and not board.checked_rows[row][column]:
         # 道
-        if board.rows[row][column] in ('┘', '└', '│', '│┌', '┐│', '┘│', '│└', '┘└'):
+        if board.rows[row][column] in ('┘', '└', '┘r', '└r', '│', '│┌', '┐│', '┘│', '│└', '┘└'):
             board.checked_rows[row][column] = True
             agent.location[1] += 1
             return True
 
-        if len(board.rows[row][column]) == 1:
+        if len(board.rows[row][column]) == 1 or board.rows[row][column] == '..':
             # 文字
             board.checked_rows[row][column] = True
             agent.location[1] += 1
@@ -82,13 +82,13 @@ def move_to_left(board, agent):
     column = agent.location[0] - 1
     row = agent.location[1]
     if column > 0 and not board.checked_rows[row][column]:
-        if board.rows[row][column] in ('┌', '└', '─', '─┌', '┐─', '┘─', '─└', '└┌', '┌└'):
+        if board.rows[row][column] in ('┌', '└', '┌r', '└r', '─', '─┌', '┐─', '┘─', '─└', '└┌', '┌└'):
             # 道
             board.checked_rows[row][column] = True
             agent.location[0] -= 1
             return True
 
-        if len(board.rows[row][column]) == 1:
+        if len(board.rows[row][column]) == 1 or board.rows[row][column] == '..':
             # 文字
             board.checked_rows[row][column] = True
             agent.location[0] -= 1
